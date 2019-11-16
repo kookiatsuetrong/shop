f73e26c3dbde5c560ef52962b05095ab8dda82c3
Kookiat <kookiatsuetrong@gmail.com>
2019-11-16 20:40:26 +0700

    Add Middleware

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// download project from https://github.com/kookiatsuetrong/shop
+// npm install express ejs mysql
 var express = require('express')
 var server  = express()
 server.listen(2000)
@@ -7,10 +9,38 @@ var mysql   = require('mysql')
 var pool    = mysql.createPool(source)
 var ejs     = require('ejs')
 server.engine('html', ejs.renderFile) // set EJS to default view engine
-server.get('/', showHome)
+
+server.get([ '/', '/home' ], showHome)
 server.get('/browse', showAll)
 server.get('/test', showTest)
 server.get('/result', showSearchResult)
+server.get('/detail', showDetail)
+
+server.use( express.static('public') )
+server.use( showError )
+
+// localhost:2000/detail?code=7
+function showDetail(req,res) {
+    var sql  = 'select * from post where code=?'
+    var info = [ req.query.code, 'welcome', 3 ]
+    pool.query(sql, info, function(error, data) {
+        var model = { }
+        if (data.length == 1) {
+            model.data = data[0]
+        } else {
+            model.data = {topic: 'Not Found', detail:''}
+        }
+        var sql1 = 'select * from photo where post=?'
+        pool.query(sql1, info, function(error, image) {
+            model.image = image
+            res.render('detail.html', model)
+        })
+    })  // <%= data.topic %>  <%= data.detail %>
+}
+
+function showError(req, res, next) {
+    res.render('error.html')
+}
 
 function showHome(req, res) {
     res.render('index.html')
